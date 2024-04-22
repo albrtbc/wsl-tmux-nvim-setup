309ee08cd4ba00287013c306cd77852c232134a1
Albert Blanco-Cuaresma <albrtbc@users.noreply.github.com>
2024-04-22 21:51:45 +0200

    fixed install script

--- bin/install.sh
+++ bin/install.sh
@@ -18,11 +18,11 @@ git clone https://github.com/albrtbc/wsl-tmux-nvim-setup.git /tmp/wsl-tmux-nvim-
 
 # Move files
 mkdir -p ~/bin/auto_install/
-cp ~/wsl-tmux-nvim-setup/auto_install/main.py ~/bin/auto_install/
+cp /tmp/wsl-tmux-nvim-setup/auto_install/main.py ~/bin/auto_install/
 chmod +x ~/bin/auto_install/main.py
 mkdir -p ~/.config/auto_install/
-cp -r ~/wsl-tmux-nvim-setup/auto_install/components/ ~/.config/auto_install/
-cp ~/wsl-tmux-nvim-setup/auto_install/components.json ~/.config/auto_install/
+cp -r /tmp/wsl-tmux-nvim-setup/auto_install/components/ ~/.config/auto_install/
+cp /tmp/wsl-tmux-nvim-setup/auto_install/components.json ~/.config/auto_install/
 
 # Add to PATH
 echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
