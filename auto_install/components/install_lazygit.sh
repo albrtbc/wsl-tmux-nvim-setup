320c2b2abff86bfde3b5c3ffdae8379615b3b416
Albert Blanco-Cuaresma <albrtbc@users.noreply.github.com>
2024-04-22 21:53:41 +0200

    added lazygit dependencies

--- auto_install/components/install_lazygit.sh
+++ auto_install/components/install_lazygit.sh
@@ -3,6 +3,9 @@ set -u
 set -e
 set -x
 
+# Install dependencies
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
