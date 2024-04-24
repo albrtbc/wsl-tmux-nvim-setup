b3b438216813310b0b735bb35a99cfa112dea072
Albert Blanco-Cuaresma <albrtbc@users.noreply.github.com>
2024-04-24 12:58:05 +0200

    added lazygit config

--- auto_install/components/install_lazygit.sh
+++ auto_install/components/install_lazygit.sh
@@ -3,6 +3,13 @@ set -u
 set -e
 set -x
 
+# Clean up
+cleanup() {
+    echo "Cleaning up..."
+    rm -rf /tmp/wsl-tmux-nvim-setup
+}
+
+trap cleanup EXIT INT TERM
 # Install dependencies
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
@@ -13,3 +20,9 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 rm -rf lazygit lazygit.tar.gz
 
+# Clone wsl-tmux-nvim-setup
+git clone https://github.com/albrtbc/wsl-tmux-nvim-setup.git /tmp/wsl-tmux-nvim-setup
+
+# Move lazy config files
+mkdir -p ~/.config/lazygit/
+cp /tmp/wsl-tmux-nvim-setup/lazygit/config.yml ~/.config/lazygit/config.yml
